fefe8decb49d45b76167a6834391f98f98d7bb48
utaware <1264051408@qq.com>
2023-12-17 08:25:27 +0800

    feat(): add code-highlight component

---
+++ src/components/VueCodeHighlight/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="code-highlight" ref="codeBlock">
+    <pre :class="languageClass">
+      <code>
+        <slot></slot>
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script>
+import 'prismjs/themes/prism-tomorrow.min.css'
+
+import './custom/index.scss'
+
+import prismjs from 'prismjs'
+
+export default {
+  name: 'VueCodeHighlight',
+  components: {},
+  data() {
+    return {}
+  },
+  props: {
+    language: {
+      type: String,
+      default: 'javascript',
+    },
+  },
+  computed: {
+    languageClass() {
+      return `language-${this.language}`
+    },
+  },
+  methods: {},
+  mounted() {
+    prismjs.highlightAllUnder(this.$refs.codeBlock)
+  },
+}
+</script>
+
+<style scoped lang="scss"></style>
